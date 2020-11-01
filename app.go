f3207d7113709eaf9899aa6703134b9ef12416d2
adilrizvi <adil9791@gmail.com>
2020-11-01 20:33:55 +0530

    fix tests

--- app.go
+++ app.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
+	"strconv"
 )
 
 type App struct {
@@ -13,19 +16,177 @@ type App struct {
 	DB     *sql.DB
 }
 
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJson(w, code, map[string]string{"error": message})
+
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "internal error")
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+
+}
+
+func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Product ID")
+		return
+	}
+
+	p := product{ID: id}
+	if err := p.getProduct(a.DB); err != nil {
+		switch err {
+
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Product Not Found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+
+		}
+
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, p)
+}
+
+func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
+	var err error
+	var count, start int
+
+	count, _ = strconv.Atoi(r.FormValue("count"))
+	start, _ = strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1 {
+		count = 10
+	}
+
+	if start < 0 {
+		start = 0
+	}
+
+	var products []product
+	products, err = getProducts(a.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, products)
+}
+
+func (a *App) createProduct(w http.ResponseWriter, r *http.Request) {
+	var p product
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := p.createProduct(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJson(w, http.StatusCreated, p)
+}
+
+func (a *App) updateProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid product id")
+		return
+	}
+
+	log.Println(r.Body)
+
+	var p product
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	log.Println(p)
+
+	defer r.Body.Close()
+
+	p.ID = id
+
+	if err := p.updateProduct(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, p)
+}
+
+func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid product id")
+		return
+	}
+
+	p := product{ID:id}
+
+
+	//TODO: deleteProduct should return 404 for non existent products
+	if err := p.deleteProduct(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+
+}
+
 func (a *App) Initialize(user, password, dbname string) {
 	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+
 
 	var err error
 	a.DB, err = sql.Open("postgres", connectionString)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error in creating db connection %s", err)
 	}
+	log.Println("DB Connection Created")
 
 	a.Router = mux.NewRouter()
+	a.initializeRoutes()
 
 }
 
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/products", a.getProducts).Methods("GET")
+	a.Router.HandleFunc("/products", a.createProduct).Methods("POST")
+	a.Router.HandleFunc("/products/{id:[0-9]+}", a.getProduct).Methods("GET")
+	a.Router.HandleFunc("/products/{id:[0-9]+}", a.updateProduct).Methods("PUT")
+	a.Router.HandleFunc("/products/{id:[0-9]+}", a.deleteProduct).Methods("DELETE")
+	a.Router.HandleFunc("/hc", healthCheck).Methods("GET")
+
+}
+
+
+func healthCheck( w http.ResponseWriter, r *http.Request) {
+	respondWithJson(w, http.StatusOK, "OK")
+}
+
 func (a *App) Run(addr string) {
+	log.Printf("starting server at :%s", addr)
+	log.Fatal(http.ListenAndServe(addr, a.Router))
 
 }
--- main_test.go
+++ main_test.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 )
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
 
 var a App
 
 func TestMain(m *testing.M) {
 	a.Initialize(os.Getenv("APP_DB_USERNAME"), os.Getenv("APP_DB_PASSWORD"), os.Getenv("APP_DB_NAME"))
-	ensureTableExists()
+	//ensureTableExists()
 	code := m.Run()
 	clearTable()
 	os.Exit(code)
@@ -28,10 +42,150 @@ func clearTable() {
 	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
 }
 
-const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
-(
-    id SERIAL,
-    name TEXT NOT NULL,
-    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
-    CONSTRAINT products_pkey PRIMARY KEY (id)
-)`
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d, Got %d", expected, actual)
+	}
+}
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+	fmt.Printf("body::%s\n", response.Body.String())
+	if body := response.Body.String(); body != "null" {
+		log.Println(body)
+		t.Errorf("Expected an empty array, Got %s", body)
+	}
+}
+
+func TestGetNonExistentProduct(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products/11", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	err := json.Unmarshal(response.Body.Bytes(), &m)
+	if err != nil {
+		log.Printf("body::%s", response.Body.String())
+		t.Errorf("error parsing response %v", response.Body.String())
+	}
+	if m["error"] != "Product Not Found" {
+		t.Errorf("Expected the 'error' set to 'Product Not Found' , Got:%v", m["error"])
+	}
+}
+
+func TestCreateProduct(t *testing.T) {
+	clearTable()
+
+	var jsonStr = []byte(`{"name": "test product", "price": 11.22}`)
+	req, _ := http.NewRequest("POST", "/products", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusCreated, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != "test product" {
+		t.Errorf("Expected Product Name 'test product', Got:%v", m["name"])
+
+	}
+
+	if m["price"] != 11.22 {
+		t.Errorf("Expected price '11.22', Got:%v", m["price"])
+	}
+
+	if m["id"] != 1.0 {
+		t.Errorf("Expected price '1.0', Got:%v", m["id"])
+	}
+
+}
+
+func addProducts(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO products(name, price) VALUES($1, $2)", "Product "+strconv.Itoa(i), (i+1.0)*10)
+	}
+}
+
+func TestFetchProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/products/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+}
+
+func TestUpdateProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/products/1", nil)
+	response := executeRequest(req)
+
+	var originalProduct map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalProduct)
+
+	var jsonStr = []byte(`{"name": "test_update_product", "price": 11.22}`)
+	req, _ = http.NewRequest("PUT", "/products/1", bytes.NewBuffer(jsonStr))
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != "test_update_product" {
+		t.Errorf("Expected updated name:'test_updated_product' , Got:%v", m["name"])
+
+	}
+	if m["price"] != 11.22 {
+		t.Errorf("Expected updated price:'11.22' , Got:%v", m["price"])
+
+	}
+
+	if m["id"] != originalProduct["id"] {
+		t.Errorf("Expected Product ID:%v to be same,Got;%v", originalProduct["id"], m["id"])
+
+	}
+
+}
+
+func TestDeleteProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/products/1", nil)
+	response := executeRequest(req)
+	executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/products/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/products/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+}
